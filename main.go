ca97e90f79b09a61b4bfea16830a59a499a6cde1
Jordan Rash <15827604+jordan-rash@users.noreply.github.com>
2023-10-24 13:31:42 -0600

    fixes upload size
    Signed-off-by: Jordan Rash <15827604+jordan-rash@users.noreply.github.com>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	echo "echo/gen"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -72,6 +73,8 @@ func (g *Echo) Handle(req echo.WasiHttpIncomingHandlerIncomingRequest, resp echo
 }
 
 func writeHttpResponse(responseOutparam echo.WasiHttpTypesResponseOutparam, statusCode uint16, inHeaders []echo.WasiHttpTypesTuple2StringListU8TT, body []byte) {
+	echo.WasiLoggingLoggingLog(echo.WasiLoggingLoggingLevelDebug(), "writeHttpResponse", "writing response: "+string(body))
+
 	headers := echo.WasiHttpTypesNewFields(inHeaders)
 
 	outgoingResponse := echo.WasiHttpTypesNewOutgoingResponse(statusCode, headers)
@@ -84,17 +87,46 @@ func writeHttpResponse(responseOutparam echo.WasiHttpTypesResponseOutparam, stat
 		return
 	}
 
-	cw := echo.WasiIoStreamsCheckWrite(outgoingStream.Val)
-	if cw.IsErr() {
-		return
+	pollable := echo.WasiIoStreamsSubscribeToOutputStream(outgoingStream.Val)
+
+	bIndex := 0
+	for bIndex < len(body) {
+		if echo.WasiPollPollPollOneoff([]uint32{pollable})[0] {
+			echo.WasiLoggingLoggingLog(echo.WasiLoggingLoggingLevelDebug(), "writeHttpResponse", fmt.Sprintf("inside loop - bIndex: %d", bIndex))
+
+			cw := echo.WasiIoStreamsCheckWrite(outgoingStream.Val)
+			if cw.IsErr() {
+				return
+			}
+
+			echo.WasiLoggingLoggingLog(echo.WasiLoggingLoggingLevelDebug(), "writeHttpResponse", fmt.Sprintf("inside loop - checkWrite: %d", cw.Val))
+
+			if int(cw.Val) > len(body) {
+				cw.Val = uint64(len(body))
+			}
+
+			echo.WasiLoggingLoggingLog(echo.WasiLoggingLoggingLevelDebug(), "writeHttpResponse", fmt.Sprintf("inside loop - writing: %d-%d", bIndex, cw.Val))
+			w := echo.WasiIoStreamsWrite(outgoingStream.Val, body[bIndex:int(cw.Val)])
+			if w.IsErr() {
+				echo.WasiLoggingLoggingLog(echo.WasiLoggingLoggingLevelError(), "writeHttpResponse", fmt.Sprintf("failed to write to stream: %v", w.UnwrapErr()))
+				return
+			}
+
+			bIndex = int(cw.Val) + 1
+		}
 	}
 
-	wf := echo.WasiIoStreamsBlockingWriteAndFlush(outgoingStream.Val, body)
-	if wf.IsErr() {
+	f := echo.WasiIoStreamsFlush(outgoingStream.Val)
+	if f.IsErr() {
+		echo.WasiLoggingLoggingLog(echo.WasiLoggingLoggingLevelError(), "writeHttpResponse", fmt.Sprintf("failed to flush to stream: %v", f.UnwrapErr()))
 		return
 	}
 
 	echo.WasiHttpTypesFinishOutgoingStream(outgoingStream.Val)
+
+	// NOTE: I dont know why we have to do these two steps
+	echo.WasiPollPollPollOneoff([]uint32{pollable})
+	echo.WasiIoStreamsCheckWrite(outgoingStream.Val)
 
 	outparm := echo.WasiHttpTypesSetResponseOutparam(responseOutparam, outgoingResponse)
 	if outparm.IsErr() {
